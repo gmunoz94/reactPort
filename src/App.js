b645da95d466c09c78e0f3c91c93ac7813e96c51
Gerardo Munoz <g.munozii@icloud.com>
2022-01-08 19:23:48 -0600

    changed app layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import Footer from './components/Footer';
-import Portfolio from './components/Portfolio';
-import AboutMe from './components/AboutMe';
-import Contact from './components/Contact';
+import Portfolio from './pages/Portfolio';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
@@ -23,37 +25,31 @@ function App() {
   },
   }
 
-  let [ active, setActive ] = useState('aboutMe');
-
-  const portfolioClick = () => {
-    setActive('portfolio');
-  }
-
-  const aboutClick = () => {
-    setActive('aboutMe');
-  }
-
-  const contactClick = () => {
-    setActive('contact')
-  }
-
     return (
       <div>
+        <Router>
         <Navbar style={styles.deep} variant="dark">
           <Container>
             <Navbar.Brand>Gerardo Munoz</Navbar.Brand>
             <Nav className="me-auto">
-              <Button variant="link" style={ active === "aboutMe" ? styles.primaryLink : styles.secondaryLink } onClick={aboutClick}>About Me</Button>
-              <Button variant="link" style={ active === "portfolio" ? styles.primaryLink : styles.secondaryLink } onClick={portfolioClick}>Portfolio</Button>
-              <Button variant="link" style={ active === "contact" ? styles.primaryLink : styles.secondaryLink } onClick={contactClick}>Contact Me</Button>
+              <Nav.Link style={styles.secondaryLink } href='/aboutMe'>About Me</Nav.Link>
+              <Nav.Link style={styles.secondaryLink } href='/Portfolio'>Portfolio</Nav.Link>
+              <Nav.Link style={styles.secondaryLink } href='/Contact'>Contact Me</Nav.Link>
               <Nav.Link style={styles.secondaryLink} href="https://drive.google.com/file/d/1gVfAEE5JCb16DiJI_7jd4J5O4x0gtwqg/view?usp=sharing" target="_blank">Resume</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
-        { active === "aboutMe" ? <AboutMe/> : <div></div> }
-        { active === "portfolio" ? <Portfolio/> : <div></div> }
-        { active === "contact" ? <Contact/> : <div></div> }
+        <Route exact path="/aboutMe">
+          <AboutMe />
+        </Route>
+        <Route exact path="/Contact">
+          <Contact />
+        </Route>
+        <Route exact path="/Portfolio">
+          <Portfolio />
+        </Route>
         <Footer />
+        </Router>
       </div>
     );
   }
